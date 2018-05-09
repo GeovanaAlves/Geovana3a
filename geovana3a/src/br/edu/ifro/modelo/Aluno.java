f6e26c26146e6a2dc2ff37cd959ef5d692c3df42
04121255283 <04121255283@JIPA-LABIN02-12.ifro.local>
2018-05-09 16:40:24 -0400

    mapeamento banco de dados

--- geovana3a/src/br/edu/ifro/modelo/Aluno.java
+++ geovana3a/src/br/edu/ifro/modelo/Aluno.java
@@ -3,9 +3,25 @@ package br.edu.ifro.modelo;
 
 import javax.persistence.Entity;
 
-@Entity
+@Entity 
 public class Aluno {
-   
+   @generatedValue(strategy.IDENTITY)
     private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
     private String nome;
 }
